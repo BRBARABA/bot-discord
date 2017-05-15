31029893c88ef708b83b38d72936e43522925fa6
JulietteB <juliette.debressieux@gmail.com>
2017-05-15 16:27:20 +0200

    Add bot token and channel

--- config.js
+++ config.js
@@ -2,3 +2,8 @@ module.exports = {
   token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
   channel: 'channelid'
 }
+
+module.exports = {
+  token: 'MzEzNjc5MTM3OTUwNzkzNzMw.C_tJ_Q.klX6J3g1tSQLSmfgA0tCaKAObio',
+  channel: '306030462613192714'
+}
