253cce65f5f7cf84a164efd1b28aad2cabc6bc6d
adebarjac <alixdebarjac@MacBook-Air-de-Alix.local>
2017-06-07 11:57:25 +0200

    Adding Twitter service

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
+const twitter = require('./services/twitter.js')
 const youtube = require('./services/youtube.js')(config.youtube_apikey)
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
+  twitter.recup_tweet(send)
 })
 
 client.on('message', msg => {
@@ -32,10 +34,28 @@ client.on('message', msg => {
     }
     youtube.search(data, answer)
   }
-})
 
-client.login(config.token)
+  if (msg.content.substring(0, 7) === '!tweet ') {
+    if (msg.content.substring(7).length <= 140 && msg.content.substring(7).length > 0) {
+      twitter.post_tweet({text: msg.content.substring(7), channel: msg.channel}, answer)
+    } else {
+      return msg.channel.sendMessage('Votre message contient plus de 140 caractères !')
+    }
+  }
+})
 
 function answer (content, channel) {
   channel.send(content)
 }
+
+function send (content) {
+  var channelID = config.channel
+  for (var channel of client.channels) {
+    if (channel[0] === channelID) {
+      channel[1].send(content)
+      return
+    }
+  }
+}
+
+client.login(config.token)
