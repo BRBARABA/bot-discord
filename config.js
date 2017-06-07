d72913f4e63f2376a02c07ddff91edd27a2693b9
Alex <alex.baertsoen@musga.fr>
2017-06-08 01:20:56 +0200

    merged files

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 module.exports = {
   // DISCORD
-  token: 'MzE2MjY0MTcyMDcxNDE5OTE1.DASveg.5IRc7izG5Tb_KIBLKZtJV6hBJLk',
-  channel: '306030462613192714',
+  discord_token: 'MzE2MjY0MTcyMDcxNDE5OTE1.DASveg.5IRc7izG5Tb_KIBLKZtJV6hBJLk',
+  discord_channel: '306030462613192714',
 
   // YOUTUBE
-  youtube_apikey: 'AIzaSyBkh2fe-WnWNZC2_833q5PyF64U51KLMa4'
+  youtube_apikey: 'AIzaSyBkh2fe-WnWNZC2_833q5PyF64U51KLMa4',
+
+  // TWITTER
+  twitter_consumer_key: '4ZgTGALardDer88G93vXqg',
+  twitter_consumer_secret: '6oXd1zkTx1SJIRABCImhVTRANLJCVBh9K96sgzV8RsU',
+  twitter_access_token_key: '521527193-ca6UrTi7uFYXN32tlZsb9oN1Gf2wcOdPwhFR42je',
+  twitter_access_token_secret: '9qq6wApoHOKh7O5V81eM3kV3EBZo5u8mM4LGcLEVGkYh5',
+  twitter_tracked: 'MusgaFR'
 }
---
+++ services/twitter.js
@@ -0,0 +1,42 @@
+var Twitter = require('twitter')
+var config = require('../config.js')
+
+var client = new Twitter({
+  consumer_key: config.twitter_consumer_key,
+  consumer_secret: config.twitter_consumer_secret,
+  access_token_key: config.twitter_access_token_key,
+  access_token_secret: config.twitter_access_token_secret
+})
+
+module.exports = {
+  post_tweet: function (data, callback) {
+    var params = {status: data.content}
+    client.post('statuses/update', params, function (error, tweets, response) {
+      var message = ''
+      if (!error) {
+        message = 'J\'ai tweeté !'
+      } else {
+        message = 'Je n\'ai pas réussi à tweeter'
+      }
+      if (callback) {
+        callback(message, data.channel)
+      }
+    })
+  },
+
+  recup_tweet: function (callback) {
+    client.stream('statuses/filter', {track: 'MusgaFR'}, function (stream) {
+      stream.on('data', function (tweet) {
+        if (callback) {
+          var message = 'Nouveau message :\nhttp://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str
+          callback(message)
+        }
+      })
+
+      stream.on('error', function (error) {
+        console.log('Twitter stream error : ', error)
+        // throw error
+      })
+    })
+  }
+}
