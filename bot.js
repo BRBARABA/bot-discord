96575c48b96245c659f25b43c962e84275bd4b6a
JulietteB <juliette.debressieux@gmail.com>
2017-06-08 16:37:42 +0200

    fix travis errors

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ var SpotifyWebApi = require('spotify-web-api-node')
 const client = new Discord.Client()
 
 var spotify = new SpotifyWebApi({
-	clientId: 'f5633eb317cf4723b85ae67903cea6b8',
-	clientSecret: 'a840ee93598c492880d92942f00e6404'
+  clientId: 'f5633eb317cf4723b85ae67903cea6b8',
+  clientSecret: 'a840ee93598c492880d92942f00e6404'
 })
 
 client.on('ready', () => {
@@ -23,7 +23,7 @@ client.on('message', msg => {
 			msg.channel.send('Hello to you too, fellow !')
 		}
 
-		// If message begins by !spotifySearch, post the complete answer
+ 		// If message begins by !spotifySearch, post the complete answer
 		if (msg.content.startsWith('!spotifySearch')) {
 			let msgContent = msg.content.trim()
 	    let msgSplit = msgContent.split(' ')
@@ -33,7 +33,7 @@ client.on('message', msg => {
 	    spotify.searchTracks(retour)
 			.then(function (data) {
 				let numberOfAnswer=0;
-				while(numberOfAnswer<3){
+				while (numberOfAnswer<3) {
 					msg.channel.send("************************")
 					msg.channel.send("The answer "+(numberOfAnswer+1)+" is:")
 					msg.channel.send("The Artist: " + data.body.tracks.items[numberOfAnswer].artists[0].name + "; The Track: " + data.body.tracks.items[numberOfAnswer].name+ "; The Album: " + data.body.tracks.items[numberOfAnswer].album.name)
@@ -45,7 +45,7 @@ client.on('message', msg => {
 		}
 
 		// If message begins by !spotifyArtists, post the artist's names
-		else if (msg.content.startsWith('!spotifyArtists')) {
+		if (msg.content.startsWith('!spotifyArtists')) {
 			let msgContent = msg.content.trim()
 	    let msgSplit = msgContent.split(' ')
 	    let index = msgSplit.indexOf('!spotifyArtists')
@@ -54,7 +54,7 @@ client.on('message', msg => {
 	    spotify.searchTracks(retour)
 			.then(function (data) {
 				let numberOfAnswer=0;
-				while(numberOfAnswer<3){
+				while (numberOfAnswer<3){
 					msg.channel.send("************************")
 					msg.channel.send("The answer "+(numberOfAnswer+1)+" is:")
 					msg.channel.send("The Artist: " + data.body.tracks.items[numberOfAnswer].artists[0].name)
@@ -65,8 +65,8 @@ client.on('message', msg => {
 			})
 		}
 
- 		// If message begins by !spotifyAlbums, post the album's names
-		else if (msg.content.startsWith('!spotifyAlbums')) {
+		// If message begins by !spotifyAlbums, post the album's names
+		if (msg.content.startsWith('!spotifyAlbums')) {
 			let msgContent = msg.content.trim()
 			let msgSplit = msgContent.split(' ')
 			let index = msgSplit.indexOf('!spotifyAlbums')
@@ -75,7 +75,7 @@ client.on('message', msg => {
 			spotify.searchTracks(retour)
 			.then(function (data) {
 				let numberOfAnswer=0;
-				while(numberOfAnswer<3){
+				while (numberOfAnswer<3){
 					msg.channel.send("************************")
 					msg.channel.send("The answer "+(numberOfAnswer+1)+" is:")
 					msg.channel.send("The Album: " + data.body.tracks.items[numberOfAnswer].album.name)
@@ -85,9 +85,8 @@ client.on('message', msg => {
 				console.error(err)
 			})
 		}
-
- 		// If message begins by !spotifyTracks, post the track's names
-		else if (msg.content.startsWith('!spotifyTracks')) {
+		// If message begins by !spotifyTracks, post the track's names
+		if (msg.content.startsWith('!spotifyTracks')) {
 			let msgContent = msg.content.trim()
 			let msgSplit = msgContent.split(' ')
 			let index = msgSplit.indexOf('!spotifyTracks')
@@ -96,7 +95,7 @@ client.on('message', msg => {
 			spotify.searchTracks(retour)
 			.then(function (data) {
 				let numberOfAnswer=0;
-				while(numberOfAnswer<3){
+				while (numberOfAnswer<3){
 					msg.channel.send("************************")
 					msg.channel.send("The answer "+(numberOfAnswer+1)+" is:")
 					msg.channel.send("The Track: " + data.body.tracks.items[numberOfAnswer].name)
